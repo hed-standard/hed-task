3b02816e9e3f5cf322cb84c749d4a7001c46788c
Kay Robbins <robbins.kay@gmail.com>
2026-04-20 09:25:11 -0500

    Generated a readme

---
+++ setup.py
@@ -0,0 +1,7 @@
+# setup.py is only needed for editable installs (pip install -e .)
+# All configuration is in pyproject.toml
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
